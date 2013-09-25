4f56d7c99a793fbeaa5c289402c32838ed249407
Chris Russell <chris.russell@havasww.com>
2013-09-25 14:18:34 +0100

    commit

--- Gruntfile.js
+++ Gruntfile.js
@@ -42,7 +42,7 @@ module.exports = function(grunt) {
 		},
 		watch: {
 			scripts: {
-				files: ['src/templates/**/*.*'],
+				files: ['src/templates/**/**'],
 				tasks: ['assemble'],
 				options: {
 					spawn: false,
