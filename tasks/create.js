22e2f8c83d1200f976ef80c2b3e9fb15dc5f6694
Alex Meah <hi@alexmeah.com>
2013-10-17 21:48:48 +0100

    Add check file to create task don't want to overwrite any hard work :)

--- tasks/create.js
+++ tasks/create.js
@@ -1,15 +1,15 @@
 module.exports = function (grunt) {
 
-	/* 
-		Create template pages and their associated assets 
+	/*
+		Create template pages and their associated assets
 		Run from the command line as follows: grunt create --name=$var
 	*/
-	
+
 	var shell = require('shelljs'),
 		name = grunt.option('name') || null;
 
 	grunt.registerTask('create', function() {
-		
+
 		var assetsDirectory = 'src/assets/',
 			pagesDirectory = 'src/templates/pages/',
 			cssDirectory = 'src/assets/styl/',
@@ -18,12 +18,18 @@ module.exports = function (grunt) {
 			modulePattern = 'var '+name+' = (function () {\n\tfunction init () {\n\t}\n\treturn {\n\t\tinit: init\n\t}\n}());\n\n$(function () {\n\t'+name+'.init();\n});',
 			templateContent = '---\ntitle: '+name+'\n---';
 
+		// Cursory check that the file doesn't exist
+		if(grunt.file.exists(pagesDirectory+name+'.hbs')) {
+			grunt.fail.warn('Sorry, that page already exists' ,1);
+		}
+
+
 		shell.exec('mkdir '+imagesDirectory+name);
 		shell.exec('touch '+javascriptDirectory+name+'.js');
 
 		// create css
 		grunt.file.write(cssDirectory+name+'.styl', '.'+name+'\n\t/* '+name+' */')
-		
+
 		// import styles into main stylsheet
 		var screenStyl = grunt.file.read(cssDirectory+'screen.styl');
 		grunt.file.write(cssDirectory+'screen.styl', screenStyl+'\n@import "'+name+'.styl"')
