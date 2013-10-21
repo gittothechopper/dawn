0f02b31b7ef1133d007ee9839be6273d7e34fbab
Chris Russell <chris.russell@havasww.com>
2013-10-21 16:35:24 +0100

    cleaning

--- api/app.js
+++ api/app.js
@@ -4,46 +4,27 @@ var express = require('express'),
 	path = require('path'),
 	app = express();
 
-// all environments
-app.set('port', process.env.PORT || 3000);
-app.set('views', __dirname + '/views');
-app.set('view engine', 'jade');
-app.use(express.favicon());
-app.use(express.logger('dev'));
-app.use(express.bodyParser());
-app.use(express.methodOverride());
 app.use(app.router);
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', routes.index);
+app.get('/', function (req, res) {
+	res.send(200);
+});
 
 app.get('/ref', function (req, res) {
-
-	var fs = require('fs-extra')
-
-	var file = './src/assets/styl/screen.styl'
+	var fs = require('fs-extra'),
+		file = './src/assets/styl/screen.styl';
 
 	content = '\n.ref1 {\n\tleft: 100px;\n\ttop: 100px;\n}'
-
 	fs.createFile(file, function (err) {
-		if (err) {
-			console.log(err);
-		}
+		if (err) { console.log(err); }
 		else {
 			fs.appendFile(file, content, function (err) { 
-				if (err) {
-					console.log(err);
-				}
-				else {
-					res.send(200);
-				}
+				if (err) { console.log(err); }
+				else { res.send(200); }
 			});
 		}
 	});
 
 });
 
-
-http.createServer(app).listen(app.get('port'), function(){
-	console.log('Express server listening on port ' + app.get('port'));
-});
+http.createServer(app).listen(3000);
