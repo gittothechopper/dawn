716999aa94257f049de48df8fee3506ac1606340
Chris Russell <chris.russell@havasww.com>
2013-10-21 18:42:12 +0100

    sigh... the data game

--- api/app.js
+++ api/app.js
@@ -4,6 +4,8 @@ var express = require('express'),
 	path = require('path'),
 	app = express();
 
+app.use(require("express-chrome-logger"));
+app.use(express.bodyParser());
 app.use(app.router);
 
 app.get('/', function (req, res) {
@@ -14,17 +16,23 @@ app.get('/ref', function (req, res) {
 	var fs = require('fs-extra'),
 		file = './src/assets/styl/screen.styl';
 
-	content = '\n.ref1 {\n\tleft: 100px;\n\ttop: 100px;\n}'
-	fs.createFile(file, function (err) {
-		if (err) { console.log(err); }
-		else {
-			fs.appendFile(file, content, function (err) { 
-				if (err) { console.log(err); }
-				else { res.send(200); }
-			});
-		}
-	});
+	// 	x = req.body.x;
 
+	// content = '\n.ref1 {\n\tleft: '+x+'px;\n\ttop: 100px;\n}';
+
+	// fs.createFile(file, function (err) {
+	// 	if (err) { console.log(err); }
+	// 	else {
+	// 		fs.appendFile(file, content, function (err) { 
+	// 			if (err) { console.log(err); }
+	// 			else { res.send(200); }
+	// 		});
+	// 	}
+	// });
+
+	var x = req.param.x;
+
+	res.send(req.query.callback+'({success:true, x:'+x+'});'); 
 });
 
 http.createServer(app).listen(3001);
