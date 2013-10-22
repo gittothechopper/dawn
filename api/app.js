4c9811c057d8f71f3146ebed05b5541efb219294
Chris Russell <chris.russell@havasww.com>
2013-10-22 18:01:42 +0100

    cors, post

--- api/app.js
+++ api/app.js
@@ -2,37 +2,27 @@ var express = require('express'),
 	routes = require('./routes'),
 	http = require('http'),
 	path = require('path'),
+	cors = require('cors'),
 	app = express();
+
+var corsOptions = {
+	origin: true
+};
 
 app.use(require("express-chrome-logger"));
 app.use(express.bodyParser());
+app.use(cors(corsOptions));
 app.use(app.router);
 
 app.get('/', function (req, res) {
 	res.send(200);
 });
 
-app.get('/ref', function (req, res) {
-	var fs = require('fs-extra'),
-		file = './src/assets/styl/screen.styl';
+app.post('/ref', function (req, res) {
 
-	// 	x = req.body.x;
+	var resData = req.body.name;
 
-	// content = '\n.ref1 {\n\tleft: '+x+'px;\n\ttop: 100px;\n}';
-
-	// fs.createFile(file, function (err) {
-	// 	if (err) { console.log(err); }
-	// 	else {
-	// 		fs.appendFile(file, content, function (err) { 
-	// 			if (err) { console.log(err); }
-	// 			else { res.send(200); }
-	// 		});
-	// 	}
-	// });
-
-	var x = req.param.x;
-
-	res.send(req.query.callback+'({success:true, x:'+x+'});'); 
+	res.send(resData);
 });
 
 http.createServer(app).listen(3001);
