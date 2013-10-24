60fed7bfa2e81a88ff58321c13f0583a46a9cd4a
Chris Russell <hello@chrisrussell.co>
2013-10-24 20:17:44 +0100

    forming ref api

--- api/routes/index.js
+++ api/routes/index.js
@@ -3,8 +3,13 @@ exports.ref = function (req, res) {
 	// modules
 	var fs = require('fs-extra');
 
-	fs.createFile(+'./meep.css', function (err) {
-		if (err) { console.log(err); }
-	});
+	// request
+	var data = req.body,
+		page = data.page,
+		reference = data.reference,
+		positionX = data.position.x,
+		positionY = data.position.y;
+
+	res.send(200);
 
 };
