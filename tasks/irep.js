a9c35b2412c78340a4cef2816172b2eeb093795d
Chris Russell <chris.russell@havasww.com>
2013-10-31 12:21:55 +0000

    ...

--- tasks/irep.js
+++ tasks/irep.js
@@ -1,3 +1,33 @@
 module.exports = function (grunt) {
-	grunt.registerTask('irep', ['string-replace']);
+	grunt.registerTask('irep', ['string-replace'], function () {
+
+		var fs = require('fs.extra');
+
+		// get list of html files
+		grunt.file.expand({
+			filter: 'isFile',
+			cwd: 'dist'
+		}, ['*.html']).forEach(function (file) {	
+			var filename = file.substr(0, file.lastIndexOf('.')) || file;
+			createFolders(filename)
+		});
+
+		// create folders for irep files
+		function createFolders (filename) {
+			var tempPath = '.tmp/assets/'+filename+'/',
+				tempDirectories = [
+					tempPath,
+					tempPath+'css/',
+					tempPath+'img/',
+					tempPath+'js/'
+				]
+
+			tempDirectories.forEach(function (tempDirectory) {
+				grunt.file.mkdir(tempDirectory);
+			});
+		
+			copyAssets(filename);
+		}
+
+	});
 }
